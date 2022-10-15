96bfcc33ebd86594c3db2c19e617952fb5c38d0a
Cute_Wisp <sweatpotato13@gmail.com>
2022-10-15 10:14:41 +0900

    feat: 🎸 add generate command

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -1,10 +1,10 @@
-import axios from "axios";
-import { CommandInteraction, EmbedBuilder } from "discord.js";
+import { AttachmentBuilder, CommandInteraction } from "discord.js";
 import {
     Discord,
     Slash,
     SlashOption,
 } from "discordx";
+import { Nyaai } from "nyaai";
 
 @Discord()
 export class Generate {
@@ -19,7 +19,13 @@ export class Generate {
 
     async generate(tag: string, interaction: CommandInteraction): Promise<void> {
         try {
-            await interaction.reply(`Not implemented`);
+            const nyaai = new Nyaai();
+            const accessToken = await (await nyaai.login()).accessToken;
+            const image = await (await nyaai.generateImage(tag, accessToken)).imageBase64;
+            const imageBuffer = Buffer.from(image, "base64");
+            const attachment = new AttachmentBuilder(imageBuffer, { name: 'image.png' })
+
+            await interaction.editReply({ content: `tag: ${tag}`, files: [attachment] });
         }
         catch (error: any) {
             console.log(error.message);
