a8e16d9f39df483069bfcfc00ddb85dc3f30c294
Cute_Wisp <sweatpotato13@gmail.com>
2022-10-16 11:15:36 +0900

    fix: 🐛 fix typeorm not applied

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity("user")
+@Entity("user", { schema: "novelai" })
 export class User {
     @PrimaryGeneratedColumn({ type: "integer", name: "id" })
     id!: number;
